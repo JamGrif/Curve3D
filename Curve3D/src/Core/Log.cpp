44825511378fb9775da7ccee98cac956690238a6
Jamie Griffiths <40271350+JamGrif@users.noreply.github.com>
2025-12-28 23:00:36 +0000

    improved logging system

--- Curve3D/src/Core/Log.cpp
+++ Curve3D/src/Core/Log.cpp
@@ -1,18 +1,20 @@
 #include "pch.h"
 
-#include "spdlog/spdlog.h"
+#include "spdlog/logger.h"
+#include "spdlog/sinks/stdout_sinks.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
 
 std::shared_ptr<spdlog::logger> Log::s_pLogger;
 
 /// <summary>
-/// Sets the text colour, timestamp, name and log message settings
+/// Create logger and set format of log message
 /// </summary>
 void Log::Init()
 {
-	//%^ colouring, [%T] timestamp, %n name of the logger, %v%$ actual log message
-	spdlog::set_pattern("%^[%T] %n: %v%$");
+	s_pLogger = spdlog::stdout_color_mt("Curve3D");
 
-	s_pLogger = spdlog::stdout_color_mt("OpenGL_Project");
 	s_pLogger->set_level(spdlog::level::trace);
+
+	//%n name of the logger, %^ colouring, %s source file, %v%$ actual log message
+	spdlog::set_pattern("%n: %^ %s %v%$");
 }
